f99ff3e5de25ce6774994a18f0cdef0f1f4ba5f6
David Nathanael <davidnathanael@davids-mbp.lan>
2021-11-01 17:53:16 -0700

    Adding files

--- scraping.py
+++ scraping.py
@@ -1,4 +1,4 @@
-
+# 10.5.2, 10.5.3
 # Import Splinter, BeautifulSoup, and Pandas
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
@@ -19,7 +19,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemispheres": hemispheres(browser)
     }
 
     # Stop webdriver and return data
@@ -101,6 +102,31 @@ def mars_facts():
     return df.to_html(classes="table table-striped")
 
 if __name__ == "__main__":
-    
     # If running as script, print scraped data
     print(scrape_all())
+    
+
+def hemispheres(browser):   
+    # 1. Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # # 3. Write code to retrieve the image urls and titles for each hemisphere.
+
+    links = browser.find_by_css("a.product-item img")
+    for x in range(len(links)):
+        hemisphere = {}
+        browser.find_by_css("a.product-item img")[x].click()
+        image = browser.links.find_by_text('Sample').first
+        hemisphere['image_url'] = image["href"]
+        hemisphere['title'] = browser.find_by_css("h2.title").text
+
+        hemisphere_image_urls.append(hemisphere)
+
+        browser.back()
+
+    print(hemisphere_image_urls)
+    return hemisphere_image_urls
